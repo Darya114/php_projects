ed123744ff44bd979a51a240190be445378dbebf
Darya <vasilchuk_darya_82@mail.ru>
2025-10-29 17:37:03 +0800

    Added control_structures.php with function placeholders

---
+++ src/control_structures.php
@@ -0,0 +1,22 @@
+<?php
+// src/control_structures.php
+
+declare(strict_types=1);
+
+function checkNumber(int|float $num): string {
+    return '';
+}
+
+function getAgeCategory(int $age): string {
+    return '';
+}
+
+function printNumbers(int $n): void {}
+
+function factorial(int $n): int {
+    return 1;
+}
+
+function printArrayItems(array $items): void {}
+
+function printEvenNumbers(int $n): void {}
