1ce66d4dddc973cf032fb1937ef300acfa0868f8
Darya <vasilchuk_darya_82@mail.ru>
2025-10-29 17:25:44 +0800

    Added arrays.php with function and class placeholders

---
+++ src/arrays.php
@@ -0,0 +1,16 @@
+<?php
+// src/arrays.php
+
+declare(strict_types=1);
+
+function filterEvenNumbers(array $numbers): array {
+	return [];
+}
+
+function squareNumbers(array $numbers): array {
+	return [];
+}
+
+function getUserEmails(array $users): array {
+    return [];
+}
