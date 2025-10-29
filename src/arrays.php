cfb5fbd7604b8d2d8c3e42e5ccf5ea59f9b32bec
Darya <vasilchuk_darya_82@mail.ru>
2025-10-29 17:29:13 +0800

    Implemented functions in arrays.php

--- src/arrays.php
+++ src/arrays.php
@@ -4,13 +4,24 @@
 declare(strict_types=1);
 
 function filterEvenNumbers(array $numbers): array {
-	return [];
+	return array_values(array_filter(
+		$numbers,
+		fn($num) => is_int($num) ? $num % 2 === 0 : throw new TypeError("All elements must be numbers")
+	));
 }
 
 function squareNumbers(array $numbers): array {
-	return [];
+	return array_map(
+		fn($num) => is_int($num) ? $num ** 2 : throw new TypeError("All elements must be numbers"),
+		$numbers
+	);
 }
 
 function getUserEmails(array $users): array {
-    return [];
+    return array_map(
+        fn($user) => is_array($user) && isset($user['email'])
+            ? $user['email']
+            : throw new TypeError("Each user must be an array with an 'email' key"),
+        $users
+    );
 }
