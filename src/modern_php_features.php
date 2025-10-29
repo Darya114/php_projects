272f938803a49da078e62069b3b36183687439ac
Darya <vasilchuk_darya_82@mail.ru>
2025-10-29 15:49:27 +0800

    Added modern_php_features.php with function and class placeholders

---
+++ src/modern_php_features.php
@@ -0,0 +1,22 @@
+<?php
+// src/modern_php_features.php
+
+function getStatusMessage(string $str): string {
+    return '';
+}
+
+function calculatePrice(fint|float $basePrice, int $discount, int $tax): string {
+    return '';
+}
+
+class User {}
+
+function getDeliveryMessage(OrderStatus $status): string {
+    return '';
+}
+
+enum OrderStatus: string {}
+
+function getUserEmail(object $user): string {
+    return '';
+}
